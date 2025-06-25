4478301e38743b55bf3b1317e612fd5cb920a71d
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2025-06-25 19:06:25 +0000

    creacion de hook

--- src/componentes/SelectorUI.tsx
+++ src/componentes/SelectorUI.tsx
@@ -1,11 +1,14 @@
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
 import Select, { type SelectChangeEvent } from '@mui/material/Select';
 
 export default function SelectorUI() {
+    const[cityInput, setCityInput] = useState<string>("");
+
     const handleChange = (event: SelectChangeEvent<string>) => {
-        alert(event.target.value)
+        setCityInput(event.target.value)
     };
 return (
    <FormControl fullWidth>
@@ -14,6 +17,7 @@ return (
          labelId="city-select-label"
          id="city-simple-select"
          label="Ciudad"
+         value={cityInput}
          onChange={handleChange}>
          <MenuItem disabled><em>Seleccione una ciudad</em></MenuItem>
          <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
@@ -21,6 +25,11 @@ return (
          <MenuItem value={"manta"}>Manta</MenuItem>
          <MenuItem value={"cuenca"}>Cuenca</MenuItem>
       </Select>
+      {cityInput && (
+            <p>
+                Información del clima en <span style={{textTransform: 'capitalize', fontWeight: 'bold'}}>{cityInput}</span>
+            </p>
+        )}
 
    </FormControl>
    )
